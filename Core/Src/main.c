08d814fa830b8d1b7e0bf3592285422ff67bd082
thammgh37 <hoanganh03052003@gmail.com>
2023-10-25 02:13:28 +0700

    ex10

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -47,15 +47,30 @@ uint16_t cot[8]= {ENM0_Pin,ENM1_Pin,ENM2_Pin,ENM3_Pin,ENM4_Pin,ENM5_Pin,ENM6_Pin
 uint16_t hang[8]= {ROW0_Pin,ROW1_Pin,ROW2_Pin,ROW3_Pin,ROW4_Pin,ROW5_Pin,ROW6_Pin,ROW7_Pin};
 const int MAX_LED_MATRIX = 8;
 int index_led_matrix = 0;
-uint8_t matrix_buffer[8] = { 0xF8,0xFC,0x36,0x33,0x33,0x36,0xFC,0xF8};
+uint8_t matrix_buffer[16][8] = {{ 0xF8,0xFC,0x36,0x33,0x33,0x36,0xFC,0xF8},
+								{ 0x00,0xF8,0xFC,0x36,0x33,0x33,0x36,0xFC},
+								{ 0x00,0x00,0xF8,0xFC,0x36,0x33,0x33,0x36},
+								{ 0x00,0x00,0x00,0xF8,0xFC,0x36,0x33,0x33},
+								{ 0x00,0x00,0x00,0x00,0xF8,0xFC,0x36,0x33},
+								{ 0x00,0x00,0x00,0x00,0x00,0xF8,0xFC,0x36},
+								{ 0x00,0x00,0x00,0x00,0x00,0x00,0xF8,0xFC},
+								{ 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0xF8},
+								{ 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00},
+								{ 0xF8,0x00,0x00,0x00,0x00,0x00,0x00,0x00},
+								{ 0xFC,0xF8,0x00,0x00,0x00,0x00,0x00,0x00},
+								{ 0x36,0xFC,0xF8,0x00,0x00,0x00,0x00,0x00},
+								{ 0x33,0x36,0xFC,0xF8,0x00,0x00,0x00,0x00},
+								{ 0x33,0x33,0x36,0xFC,0xF8,0x00,0x00,0x00},
+								{ 0x36,0x33,0x33,0x36,0xFC,0xF8,0x00,0x00},
+								{ 0xFC,0x36,0x33,0x33,0x36,0xFC,0xF8,0x00}};
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-static void MX_TIM2_Init(void);
 static void MX_GPIO_Init(void);
+static void MX_TIM2_Init(void);
 /* USER CODE BEGIN PFP */
-void updateLEDMatrix(int index);
+void updateLEDMatrix(int index1,int index2);
 void writeByte(uint16_t* pins, uint8_t byte, GPIO_TypeDef* port);
 /* USER CODE END PFP */
 
@@ -91,29 +106,33 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
-  MX_TIM2_Init();
   MX_GPIO_Init();
+  MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
   HAL_TIM_Base_Start_IT(&htim2);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  setTimer1(100);
-  updateLEDMatrix(7);
+  setTimer1(10);
+  setTimer2(100);
+  updateLEDMatrix(0,7);
   int index_led = 0;
+  int status_trans = 0;
   while (1)
   {
 	  if (timer1_flag == 1){
-		  updateLEDMatrix(index_led++);
-		  if (index_led == 8){
-			  setTimer1(10);
-		  }
-		  else if (index_led < 8){
-			  setTimer1(5);
-		  }
-		  if ( index_led >= 8 ){
+		  setTimer1(10);
+		  updateLEDMatrix(status_trans,index_led++);
+		  if (index_led >=8){
 			  index_led = 0;
+		  }
+	  }
+	  if (timer2_flag == 1){
+		  setTimer2(100);
+		  status_trans++;
+		  if (status_trans >=16){
+			  status_trans = 0;
 		  }
 	  }
     /* USER CODE END WHILE */
@@ -217,10 +236,10 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, ENM0_Pin|ENM1_Pin|DOT_Pin|LED_Pin
-                          |EN0_Pin|EN1_Pin|EN2_Pin|EN3_Pin
-                          |ENM2_Pin|ENM3_Pin|ENM4_Pin|ENM5_Pin
-                          |ENM6_Pin|ENM7_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, ENM0_Pin|ENM1_Pin|DOT_Pin|EN0_Pin
+                          |EN1_Pin|EN2_Pin|EN3_Pin|ENM2_Pin
+                          |ENM3_Pin|ENM4_Pin|ENM5_Pin|ENM6_Pin
+                          |ENM7_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, SEG0_Pin|SEG1_Pin|SEG2_Pin|ROW2_Pin
@@ -228,14 +247,14 @@ static void MX_GPIO_Init(void)
                           |ROW7_Pin|SEG3_Pin|SEG4_Pin|SEG5_Pin
                           |SEG6_Pin|ROW0_Pin|ROW1_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : ENM0_Pin ENM1_Pin DOT_Pin LED_Pin
-                           EN0_Pin EN1_Pin EN2_Pin EN3_Pin
-                           ENM2_Pin ENM3_Pin ENM4_Pin ENM5_Pin
-                           ENM6_Pin ENM7_Pin */
-  GPIO_InitStruct.Pin = ENM0_Pin|ENM1_Pin|DOT_Pin|LED_Pin
-                          |EN0_Pin|EN1_Pin|EN2_Pin|EN3_Pin
-                          |ENM2_Pin|ENM3_Pin|ENM4_Pin|ENM5_Pin
-                          |ENM6_Pin|ENM7_Pin;
+  /*Configure GPIO pins : ENM0_Pin ENM1_Pin DOT_Pin EN0_Pin
+                           EN1_Pin EN2_Pin EN3_Pin ENM2_Pin
+                           ENM3_Pin ENM4_Pin ENM5_Pin ENM6_Pin
+                           ENM7_Pin */
+  GPIO_InitStruct.Pin = ENM0_Pin|ENM1_Pin|DOT_Pin|EN0_Pin
+                          |EN1_Pin|EN2_Pin|EN3_Pin|ENM2_Pin
+                          |ENM3_Pin|ENM4_Pin|ENM5_Pin|ENM6_Pin
+                          |ENM7_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -270,39 +289,39 @@ void writeByte(uint16_t* pins, uint8_t byte, GPIO_TypeDef* port){
 	HAL_GPIO_WritePin(port, pins[6], byte & (uint8_t)0x40);
 	HAL_GPIO_WritePin(port, pins[7], byte & (uint8_t)0x80);
 }
-void updateLEDMatrix(int index){
-	switch (index){
+void updateLEDMatrix(int index1, int index2){
+	switch (index2){
 	case 0:
 		writeByte(cot, ~0X01, ENM0_GPIO_Port);
-		writeByte(hang, ~matrix_buffer[index], ROW0_GPIO_Port);
+		writeByte(hang, ~matrix_buffer[index1][index2], ROW0_GPIO_Port);
 		break;
 	case 1:
 		writeByte(cot, ~0X02, ENM0_GPIO_Port);
-		writeByte(hang, ~matrix_buffer[index], ROW0_GPIO_Port);
+		writeByte(hang, ~matrix_buffer[index1][index2], ROW0_GPIO_Port);
 		break;
 	case 2:
 		writeByte(cot, ~0X04, ENM0_GPIO_Port);
-		writeByte(hang, ~matrix_buffer[index], ROW0_GPIO_Port);
+		writeByte(hang, ~matrix_buffer[index1][index2], ROW0_GPIO_Port);
 		break;
 	case 3:
 		writeByte(cot, ~0X08, ENM0_GPIO_Port);
-		writeByte(hang, ~matrix_buffer[index], ROW0_GPIO_Port);
+		writeByte(hang, ~matrix_buffer[index1][index2], ROW0_GPIO_Port);
 		break;
 	case 4:
 		writeByte(cot, ~0X10, ENM0_GPIO_Port);
-		writeByte(hang, ~matrix_buffer[index], ROW0_GPIO_Port);
+		writeByte(hang, ~matrix_buffer[index1][index2], ROW0_GPIO_Port);
 		break;
 	case 5:
 		writeByte(cot, ~0X20, ENM0_GPIO_Port);
-		writeByte(hang, ~matrix_buffer[index], ROW0_GPIO_Port);
+		writeByte(hang, ~matrix_buffer[index1][index2], ROW0_GPIO_Port);
 		break;
 	case 6:
 		writeByte(cot, ~0X40, ENM0_GPIO_Port);
-		writeByte(hang, ~matrix_buffer[index], ROW0_GPIO_Port);
+		writeByte(hang, ~matrix_buffer[index1][index2], ROW0_GPIO_Port);
 		break;
 	case 7:
 		writeByte(cot, (uint8_t)~0X80, ENM0_GPIO_Port);
-		writeByte(hang, ~matrix_buffer[index], ROW0_GPIO_Port);
+		writeByte(hang, ~matrix_buffer[index1][index2], ROW0_GPIO_Port);
 		break;
 	default:
 		break;
